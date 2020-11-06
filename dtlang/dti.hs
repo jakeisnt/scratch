63af53c39a34203854fc0072dda32ed77942440e
jakeisnt <jakechv@github.com>
2020-11-06 12:23:13 +0100

    Add dtlang stub

---
+++ dtlang/dti.hs
@@ -0,0 +1,17 @@
+type x = y | z
+
+lookupType :: x ctx -> cadr
+
+lookupValue ::  x ctx -> ls
+
+extend :: xtx v t -> v
+
+subst :: x
+
+normalize :: x
+
+ctx :: x
+
+inferType :: x
+
+same :: e1 -> e2 -> Bool
